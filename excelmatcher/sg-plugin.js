af4cfd5cdf5252debc42ca0d9a239130bdb016e6
Subitan Ganguly <101342918+subitanganguly@users.noreply.github.com>
2026-05-01 14:29:33 +0530

    Add files via upload

--- excelmatcher/sg-plugin.js
+++ excelmatcher/sg-plugin.js
@@ -247,7 +247,7 @@ document.head.appendChild(style);
   const container = document.createElement("div");
   container.style.position = "fixed";
   container.style.top = "20px";
-  container.style.right = "20px";
+  container.style.left = "20px";
   container.style.zIndex = "9999";
   document.body.appendChild(container);
 
