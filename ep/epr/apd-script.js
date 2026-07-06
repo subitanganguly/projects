5717bebe52aaec29907dff9077e5264178466678
Subitan Ganguly <101342918+subitanganguly@users.noreply.github.com>
2026-07-06 13:58:40 +0530

    Add files via upload

---
+++ ep/epr/apd-script.js
@@ -0,0 +1,10 @@
+// ---------- GLOBAL CONST ----------
+window.SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwzhMil50nUXVU9yyfCsbaL9anqRsIveqrh3NblUM6tcCQaesQOwZOmQsVv0zkh8-dh/exec";
+window.psList = [
+  {btn:"APD", value:"Alipurduar"}, {btn:"APD W", value:"Alipurduar Women"},
+  {btn:"KMG", value:"Kumargram"}, {btn:"KCN", value:"Kalchini"},
+  {btn:"SMT", value:"Samuktala"}, {btn:"BRP", value:"Birpara"},
+  {btn:"FKT", value:"Falakata"}, {btn:"MDT", value:"Madarihat"},
+  {btn:"JGN", value:"Jaigaon"}, {btn:"CC", value:"Cyber Crime"},
+  {btn:"GRPS", value:"APD JN GRPS"}
+];
---
+++ ep/epr/jpg-script.js
@@ -0,0 +1,15 @@
+window.SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx5v2gHEZURaWKxjfV26M1xMZzrYxIEFVIX9h27yQDLGwJ6MBmN-P32EzSnk2KBmuTgVA/exec";
+window.psList = [
+  { btn: "BANT", value: "Banarhat" },
+  { btn: "CYBER", value: "Cyber Crime" },
+  { btn: "DPG", value: "Dhupguri" },
+  { btn: "KOT", value: "Kotwali" },
+  { btn: "MET", value: "Metelli" },
+  { btn: "MNG", value: "Mainaguri" },
+  { btn: "MAL", value: "Malbazar" },
+  { btn: "NAG", value: "Nagrakata" },
+  { btn: "RAJ", value: "Rajganj" },
+  { btn: "W.PS", value: "Women PS Jalpaiguri" },
+  { btn: "BHK", value: "Bhakti Nagar" },
+  { btn: "NJP", value: "New Jalpaiguri PS" }
+];
